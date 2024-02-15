96570d5ab1e253addd763e09455b9296edba7399
beppumisato <lacuisinedemisato@gmail.com>
2024-02-15 12:11:14 +0900

    todoアプリ開発練習

---
+++ src/api.ts
@@ -0,0 +1,49 @@
+import { Task } from "./types";
+
+const baseUrl = "http// localhost:3001";
+
+export const getAllTodos = async (): Promise<Task[]> => {
+    const res = await fetch(`http://localhost:3001/tasks`,{
+        cache: "no-store", //SSR
+    });
+    const todos = await res.json();
+    return todos;
+};
+
+export const addTodo = async (todo: Task): Promise<Task> => {
+    const res = await fetch(`http://localhost:3001/tasks`,{
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todo),
+    });
+    const newTodo = await res.json();
+    return newTodo;
+};
+
+export const editTodo = async (
+    id: string, 
+    newText: string): 
+    Promise<Task> => {
+    const res = await fetch(`http://localhost:3001/tasks/${id}`,{
+        method: "PUT",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({text: newText }),
+    });
+    const updatedTodo = await res.json();
+    return updatedTodo;
+};
+
+export const deleteTodo = async (id: string): Promise<Task> => {
+    const res = await fetch(`http://localhost:3001/tasks/${id}`,{
+        method: "DELETET",
+        headers: {
+        "Content-Type": "application/json",
+        },
+    });
+    const deleteTodo = await res.json();
+    return deleteTodo;
+};
