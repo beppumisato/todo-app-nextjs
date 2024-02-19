726695977ae750904b8d145f64d0fbe5a2729678
beppumisato <lacuisinedemisato@gmail.com>
2024-02-19 13:28:00 +0900

    完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import TodoList from "@/components/TodoList";
 import Image from "next/image";
 
 export default async function Home() {
-  const todos = await getAllTodos();
+  // const todos = await getAllTodos();
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-orange-100">
       <h1 className="text-4xl font-bold text-orange-700 mt-20">
         Nextjs 13 Todo App
-        </h1>
+      </h1>
       <div className="w-full max-w-xl mt-5">
         <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
           <AddTask />
-          <TodoList todos={todos} />
+          {/* <TodoList todos={todos} /> */}
         </div>
       </div>
     </main>
